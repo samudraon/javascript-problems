9200ef4088fd243c5a83e126d4a4ecf9a922d722
samudraon <samudraon@gmailcom>
2023-02-03 03:14:22 +0600

    duplicate problem completed

---
+++ duplicate.js
@@ -0,0 +1,15 @@
+const names = ['salman', 'farhan', 'hasib', 'mamun', 'nahid', 'salman', 'mamun', 'nahid'];
+
+function removeDuplicate(names) {
+    const unique = [];
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i];
+        if (unique.includes(name) === false) {
+            unique.push(name);
+        }
+    }
+    return unique;
+}
+
+const uniqueNames = removeDuplicate(names);
+console.log(uniqueNames);
